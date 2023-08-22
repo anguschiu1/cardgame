ba39e0a5d05e56ae25f552fa430d3f19bca9a21f
Angus Chiu <anguschiu1@users.noreply.github.com>
2023-08-22 18:55:35 +0100

    implementing generate_by_prime

---
+++ src/card.rs
@@ -0,0 +1,192 @@
+#![allow(missing_docs)]
+#![warn(rustdoc::private_doc_tests)]
+
+//! Support library to define cards in `cardgame` crate.
+//!
+//! Provide structures and enums to define SpotIt! cards in the `cardgame` crate.
+//! Auxillary functions to define French cards, but it is for testing purposes only.
+//!
+//! [`Repository`]: https://github.com/anguschiu1/cardgame
+//! [`SpotIt!`]: https://www.amazon.com/Asmodee-SP411-Spot-It/dp/B0039S7NO6
+//! [`French Card Game`]: https://en.wikipedia.org/wiki/French_playing_cards
+//! [`SpotIt! Rules`]: https://www.ultraboardgames.com/spot-it/game-rules.php
+
+use std::collections::HashSet;
+use strum_macros::EnumIter;
+
+/// This defines the suits of the French Card Game.
+#[derive(EnumIter, Debug, PartialEq, Clone, Copy, Eq, Hash)]
+pub enum FrenchSuit {
+    /// This is a club, not a clover. The number 1 is not used in the French Card Game, but it is for the ease to compare power of suits in French cards.
+    Club = 1,
+    /// This is a diamond.
+    Diamond,
+    /// This is a heart.
+    Heart,
+    /// This is a spade.
+    Spade,
+}
+/// This defines the ranks of the French Card Game.
+#[derive(EnumIter, Debug, PartialEq, Clone, Copy, Eq, Hash)]
+pub enum FrenchRank {
+    Two = 2,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace, // will be 14
+}
+#[derive(EnumIter, Debug, PartialEq, Clone, Copy, Eq, Hash)]
+/// Thie enum defines the suits (or pattern) of the SpotIt! game. One card can has one or more suits.
+pub enum SpotItSymbol {
+    Apple,
+    Banana,
+    Bread,
+    Broccoli,
+    Carrot,
+    Cheese,
+    Chicken,
+    Corn,
+    Egg,
+    Fish,
+    Grape,
+    Hamburger,
+    Hotdog,
+    Icecream,
+    Lettuce,
+    Milk,
+    Mushroom,
+    Onion,
+    Orange,
+    Pea,
+    Pear,
+    Pepper,
+    Pineapple,
+    Pizza,
+    Potato,
+    Pumpkin,
+    Strawberry,
+    Tomato,
+    Watermelon,
+    Yogurt,
+}
+
+/// This tuple struct defines a French Card.
+#[derive(Debug, PartialEq, Clone, Eq, Hash)]
+pub struct FrenchCard(pub FrenchRank, pub FrenchSuit);
+
+impl FrenchCard {
+    /// This function returns the rank of the card.
+    pub fn rank(&self) -> FrenchRank {
+        self.0
+    }
+    /// This function returns the suit of the card.
+    pub fn suit(&self) -> FrenchSuit {
+        self.1
+    }
+    /// This function returns true if the suit of the card is the same as the suit of the other card.
+    pub fn match_suit(&self, card: &Self) -> bool {
+        self.1 == card.1
+    }
+    /// This function returns true if the rank of the card is the same as the rank of the other card.
+    pub fn match_rank(&self, card: &Self) -> bool {
+        self.0 == card.0
+    }
+}
+
+/// This tuple struct defines a SpotIt Card.
+/// Please notice that a SpotIt Card can have 0, 1, or more than one suits. For example, a card can have both Apple and Banana suits.
+#[derive(Debug, PartialEq, Clone, Eq)]
+pub struct SpotItCard(pub HashSet<SpotItSymbol>);
+impl SpotItCard {
+    /// This function returns true if the two cards have exactly one suit in common.
+    /// This is the key rule of the SpotIt game.
+    pub fn match_exactly_one_suit(&self, card: &Self) -> bool {
+        self.0.intersection(&card.0).count() == 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn french_rank_is_cast_as_u8() {
+        let ten = FrenchRank::Ten as u8;
+        assert_eq!(ten, 10);
+        let ace = FrenchRank::Ace as u8;
+        assert_eq!(ace, 14);
+    }
+    #[test]
+    fn can_compare_frenchcard() {
+        let card1 = FrenchCard(FrenchRank::Ace, FrenchSuit::Spade);
+        let card2 = FrenchCard(FrenchRank::Ace, FrenchSuit::Spade);
+        let card3 = FrenchCard(FrenchRank::Ace, FrenchSuit::Heart);
+        let card4 = FrenchCard(FrenchRank::King, FrenchSuit::Heart);
+        assert_eq!(card1, card2); // two cards with same rank and suit
+        assert_ne!(card2, card3); // two cards with same rank but different suit
+        assert_ne!(card3, card4); // two cards with same suit but different rank
+    }
+    #[test]
+    fn can_compare_frenchcard_by_rank_and_suit() {
+        let card1 = FrenchCard(FrenchRank::Ace, FrenchSuit::Spade);
+        let card2 = FrenchCard(FrenchRank::Ace, FrenchSuit::Spade);
+        let card3 = FrenchCard(FrenchRank::Ace, FrenchSuit::Heart);
+        let card4 = FrenchCard(FrenchRank::King, FrenchSuit::Heart);
+        assert!(card1.match_rank(&card2)); // two cards with same rank and suit
+        assert!(card1.match_rank(&card3)); // two cards with same rank but different suit
+        assert!(!card1.match_rank(&card4)); // two cards with different rank and suit
+        assert!(!card3.match_rank(&card4)); // two cards with different rank but same suit
+
+        assert!(card1.match_suit(&card2)); // two cards with same rank and suit
+        assert!(card3.match_suit(&card4)); // two cards with same suit but different rank
+        assert!(!card1.match_suit(&card3)); // two cards with same rank but different suit
+        assert!(!card1.match_suit(&card4)); // two cards with different suit and rank
+    }
+    #[test]
+    fn can_compare_spotitcard() {
+        let card1 = SpotItCard(HashSet::from([SpotItSymbol::Potato]));
+        let card2 = SpotItCard(HashSet::from([SpotItSymbol::Potato]));
+        let card3 = SpotItCard(HashSet::from([SpotItSymbol::Apple]));
+        let card4: SpotItCard =
+            SpotItCard(HashSet::from([SpotItSymbol::Potato, SpotItSymbol::Apple]));
+        let card5 = SpotItCard(HashSet::from([SpotItSymbol::Potato, SpotItSymbol::Apple]));
+        let card6 = SpotItCard(HashSet::from([SpotItSymbol::Apple, SpotItSymbol::Apple]));
+        assert_eq!(card1, card2); // two cards with same one suit
+        assert_ne!(card2, card3); // two cards with different suits
+        assert_ne!(card3, card4); // two cards with non-identical set of suit
+        assert_ne!(card1, card4); // two cards with non-identical set of suit
+        assert_eq!(card4, card5); // two cards with same set of suit
+        assert_eq!(card3, card6); // two cards with same suit, even one suit is added twice
+    }
+
+    #[test]
+    fn can_match_exactly_one_suit_on_two_spotitcard() {
+        let card1 = SpotItCard(HashSet::from([SpotItSymbol::Potato]));
+        let card2: SpotItCard =
+            SpotItCard(HashSet::from([SpotItSymbol::Potato, SpotItSymbol::Apple]));
+        let card3: SpotItCard =
+            SpotItCard(HashSet::from([SpotItSymbol::Potato, SpotItSymbol::Apple]));
+        let card4: SpotItCard = SpotItCard(HashSet::from([SpotItSymbol::Yogurt]));
+        let card5 = SpotItCard(HashSet::new());
+        let card6 = SpotItCard(HashSet::new());
+        assert!(card1.match_exactly_one_suit(&card2));
+        assert!(card2.match_exactly_one_suit(&card1));
+        assert!(!card3.match_exactly_one_suit(&card2)); // cards that have more than one suit in common
+        assert!(!card4.match_exactly_one_suit(&card3)); // cards that have no suit in common
+        assert!(!card5.match_exactly_one_suit(&card6)); // cards that have no suit in common as they are both empty
+    }
+    #[test]
+    fn can_add_suit_to_spotitcard() {
+        let mut card1 = SpotItCard(HashSet::from([SpotItSymbol::Potato]));
+        card1.0.insert(SpotItSymbol::Apple);
+        let card2 = SpotItCard(HashSet::from([SpotItSymbol::Potato]));
+        assert_ne!(card1, card2);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -11,75 +11,14 @@
 //! [`French Card Game`]: https://en.wikipedia.org/wiki/French_playing_cards
 //! [`SpotIt! Rules`]: https://www.ultraboardgames.com/spot-it/game-rules.php
 
+pub mod card;
+use card::{FrenchCard, FrenchRank, FrenchSuit, SpotItCard, SpotItSymbol};
+
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use std::collections::HashSet;
 use strum::IntoEnumIterator;
-use strum_macros::EnumIter;
 
-/// This defines the suits of the French Card Game.
-#[derive(EnumIter, Debug, PartialEq, Clone, Copy, Eq, Hash)]
-pub enum FrenchSuit {
-    /// This is a club, not a clover. The number 1 is not used in the French Card Game, but it is for the ease to compare power of suits in French cards.
-    Club = 1,
-    /// This is a diamond.
-    Diamond,
-    /// This is a heart.
-    Heart,
-    /// This is a spade.
-    Spade,
-}
-/// This defines the ranks of the French Card Game.
-#[derive(EnumIter, Debug, PartialEq, Clone, Copy, Eq, Hash)]
-pub enum FrenchRank {
-    Two = 2,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
-    Jack,
-    Queen,
-    King,
-    Ace, // will be 14
-}
-#[derive(EnumIter, Debug, PartialEq, Clone, Copy, Eq, Hash)]
-/// Thie enum defines the suits (or pattern) of the SpotIt! game. One card can has one or more suits.
-pub enum SpotItSuit {
-    Apple,
-    Banana,
-    Bread,
-    Broccoli,
-    Carrot,
-    Cheese,
-    Chicken,
-    Corn,
-    Egg,
-    Fish,
-    Grape,
-    Hamburger,
-    Hotdog,
-    Icecream,
-    Lettuce,
-    Milk,
-    Mushroom,
-    Onion,
-    Orange,
-    Pea,
-    Pear,
-    Pepper,
-    Pineapple,
-    Pizza,
-    Potato,
-    Pumpkin,
-    Strawberry,
-    Tomato,
-    Watermelon,
-    Yogurt,
-}
 /// This trait defines the common functionality of a deck of cards.
 pub trait Deck<T> {
     /// This function creates a new, empty deck of cards.
@@ -90,41 +29,15 @@ pub trait Deck<T> {
     fn shuffle(&mut self);
     /// This function pops one card from the deck of cards.
     fn pop_card(&mut self) -> Option<T>;
-}
-/// This tuple struct defines a French Card.
-#[derive(Debug, PartialEq, Clone, Eq, Hash)]
-pub struct FrenchCard(FrenchRank, FrenchSuit);
+    /// This function pushes one card to the deck of cards.
+    fn push_card(&mut self, card: T);
 
-impl FrenchCard {
-    /// This function returns the rank of the card.
-    pub fn rank(&self) -> FrenchRank {
-        self.0
-    }
-    /// This function returns the suit of the card.
-    pub fn suit(&self) -> FrenchSuit {
-        self.1
-    }
-    /// This function returns true if the suit of the card is the same as the suit of the other card.
-    pub fn match_suit(&self, card: &Self) -> bool {
-        self.1 == card.1
-    }
-    /// This function returns true if the rank of the card is the same as the rank of the other card.
-    pub fn match_rank(&self, card: &Self) -> bool {
-        self.0 == card.0
-    }
+    /// This function returns the number of cards in the deck.
+    fn len(&self) -> usize;
+    /// This function returns the number of cards in the deck.
+    fn is_empty(&self) -> bool;
 }
 
-/// This tuple struct defines a SpotIt Card.
-/// Please notice that a SpotIt Card can have 0, 1, or more than one suits. For example, a card can have both Apple and Banana suits.
-#[derive(Debug, PartialEq, Clone, Eq)]
-pub struct SpotItCard(HashSet<SpotItSuit>);
-impl SpotItCard {
-    /// This function returns true if the two cards have exactly one suit in common.
-    /// This is the key rule of the SpotIt game.
-    pub fn match_exactly_one_suit(&self, card: &Self) -> bool {
-        self.0.intersection(&card.0).count() == 1
-    }
-}
 /// This struct defines a deck of French Cards.
 #[derive(Debug)]
 pub struct FrenchDeck {
@@ -138,6 +51,10 @@ pub struct SpotItDeck {
     pub cards: Vec<SpotItCard>,
 }
 impl Deck<FrenchCard> for FrenchDeck {
+    fn push_card(&mut self, card: FrenchCard) {
+        self.cards.push(card);
+    }
+
     fn default() -> Self {
         let mut deck = Self::new();
         for suit in FrenchSuit::iter() {
@@ -158,12 +75,21 @@ impl Deck<FrenchCard> for FrenchDeck {
     fn pop_card(&mut self) -> Option<FrenchCard> {
         self.cards.pop()
     }
+    fn is_empty(&self) -> bool {
+        self.cards.is_empty()
+    }
+    fn len(&self) -> usize {
+        self.cards.len()
+    }
 }
 
 impl Deck<SpotItCard> for SpotItDeck {
+    fn push_card(&mut self, card: SpotItCard) {
+        self.cards.push(card);
+    }
     fn default() -> Self {
         let mut deck = Self::new();
-        for suit in SpotItSuit::iter() {
+        for suit in SpotItSymbol::iter() {
             let mut card = SpotItCard(HashSet::new());
             card.0.insert(suit);
             deck.cards.push(card);
@@ -177,9 +103,36 @@ impl Deck<SpotItCard> for SpotItDeck {
     fn shuffle(&mut self) {
         self.cards.shuffle(&mut thread_rng());
     }
-
     fn pop_card(&mut self) -> Option<SpotItCard> {
         self.cards.pop()
+    }
+    fn is_empty(&self) -> bool {
+        self.cards.is_empty()
+    }
+    fn len(&self) -> usize {
+        self.cards.len()
+    }
+}
+
+impl SpotItDeck {
+    fn generate_by_prime(n: u8) -> Result<SpotItDeck, String> {
+        if SpotItSymbol::iter().count() < (n * n + n + 1) as usize {
+            Err("n is too large, hence not enough symobls to generate deck.".to_string())
+        } else {
+            let mut deck = SpotItDeck::new();
+            let count = (n * n + 1) as usize;
+            let symbols_per_card: usize = (n + 1).into();
+
+            for i in 0..count {
+                let mut card = SpotItCard(HashSet::new());
+                for j in 0..symbols_per_card {
+                    card.0
+                        .insert(SpotItSymbol::iter().nth(i + j * n as usize).unwrap());
+                }
+                deck.cards.push(card);
+            }
+            Ok(deck)
+        }
     }
 }
 
@@ -196,39 +149,6 @@ mod tests {
         let deck: FrenchDeck = FrenchDeck::default();
         let mut uniq = HashSet::new();
         assert!(deck.cards.iter().all(move |card| uniq.insert(card)));
-    }
-    #[test]
-    fn french_rank_is_cast_as_u8() {
-        let ten = FrenchRank::Ten as u8;
-        assert_eq!(ten, 10);
-        let ace = FrenchRank::Ace as u8;
-        assert_eq!(ace, 14);
-    }
-    #[test]
-    fn can_compare_frenchcard() {
-        let card1 = FrenchCard(FrenchRank::Ace, FrenchSuit::Spade);
-        let card2 = FrenchCard(FrenchRank::Ace, FrenchSuit::Spade);
-        let card3 = FrenchCard(FrenchRank::Ace, FrenchSuit::Heart);
-        let card4 = FrenchCard(FrenchRank::King, FrenchSuit::Heart);
-        assert_eq!(card1, card2); // two cards with same rank and suit
-        assert_ne!(card2, card3); // two cards with same rank but different suit
-        assert_ne!(card3, card4); // two cards with same suit but different rank
-    }
-    #[test]
-    fn can_compare_frenchcard_by_rank_and_suit() {
-        let card1 = FrenchCard(FrenchRank::Ace, FrenchSuit::Spade);
-        let card2 = FrenchCard(FrenchRank::Ace, FrenchSuit::Spade);
-        let card3 = FrenchCard(FrenchRank::Ace, FrenchSuit::Heart);
-        let card4 = FrenchCard(FrenchRank::King, FrenchSuit::Heart);
-        assert!(card1.match_rank(&card2)); // two cards with same rank and suit
-        assert!(card1.match_rank(&card3)); // two cards with same rank but different suit
-        assert!(!card1.match_rank(&card4)); // two cards with different rank and suit
-        assert!(!card3.match_rank(&card4)); // two cards with different rank but same suit
-
-        assert!(card1.match_suit(&card2)); // two cards with same rank and suit
-        assert!(card3.match_suit(&card4)); // two cards with same suit but different rank
-        assert!(!card1.match_suit(&card3)); // two cards with same rank but different suit
-        assert!(!card1.match_suit(&card4)); // two cards with different suit and rank
     }
     #[test]
     fn can_push_pop_same_card_on_frenchdeck() {
@@ -250,43 +170,6 @@ mod tests {
         let deck: SpotItDeck = SpotItDeck::default();
         assert_eq!(deck.cards.len(), 30);
     }
-    #[test]
-    fn can_compare_spotitcard() {
-        let card1 = SpotItCard(HashSet::from([SpotItSuit::Potato]));
-        let card2 = SpotItCard(HashSet::from([SpotItSuit::Potato]));
-        let card3 = SpotItCard(HashSet::from([SpotItSuit::Apple]));
-        let card4: SpotItCard = SpotItCard(HashSet::from([SpotItSuit::Potato, SpotItSuit::Apple]));
-        let card5 = SpotItCard(HashSet::from([SpotItSuit::Potato, SpotItSuit::Apple]));
-        let card6 = SpotItCard(HashSet::from([SpotItSuit::Apple, SpotItSuit::Apple]));
-        assert_eq!(card1, card2); // two cards with same one suit
-        assert_ne!(card2, card3); // two cards with different suits
-        assert_ne!(card3, card4); // two cards with non-identical set of suit
-        assert_ne!(card1, card4); // two cards with non-identical set of suit
-        assert_eq!(card4, card5); // two cards with same set of suit
-        assert_eq!(card3, card6); // two cards with same suit, even one suit is added twice
-    }
-
-    #[test]
-    fn can_match_exactly_one_suit_on_two_spotitcard() {
-        let card1 = SpotItCard(HashSet::from([SpotItSuit::Potato]));
-        let card2: SpotItCard = SpotItCard(HashSet::from([SpotItSuit::Potato, SpotItSuit::Apple]));
-        let card3: SpotItCard = SpotItCard(HashSet::from([SpotItSuit::Potato, SpotItSuit::Apple]));
-        let card4: SpotItCard = SpotItCard(HashSet::from([SpotItSuit::Yogurt]));
-        let card5 = SpotItCard(HashSet::new());
-        let card6 = SpotItCard(HashSet::new());
-        assert!(card1.match_exactly_one_suit(&card2));
-        assert!(card2.match_exactly_one_suit(&card1));
-        assert!(!card3.match_exactly_one_suit(&card2)); // cards that have more than one suit in common
-        assert!(!card4.match_exactly_one_suit(&card3)); // cards that have no suit in common
-        assert!(!card5.match_exactly_one_suit(&card6)); // cards that have no suit in common as they are both empty
-    }
-    #[test]
-    fn can_add_suit_to_spotitcard() {
-        let mut card1 = SpotItCard(HashSet::from([SpotItSuit::Potato]));
-        card1.0.insert(SpotItSuit::Apple);
-        let card2 = SpotItCard(HashSet::from([SpotItSuit::Potato]));
-        assert_ne!(card1, card2);
-    }
 
     #[test]
     fn every_card_on_default_spotitdeck_is_unique() {
@@ -299,14 +182,14 @@ mod tests {
     #[test]
     fn can_push_pop_same_card_on_spotitdeck() {
         let mut deck: SpotItDeck = SpotItDeck::new();
-        let card = SpotItCard(HashSet::from([SpotItSuit::Potato]));
+        let card = SpotItCard(HashSet::from([SpotItSymbol::Potato]));
         deck.cards.push(card.clone());
         assert_eq!(deck.cards.pop(), Some(card));
     }
     #[test]
     fn can_use_fn_to_pop_card_from_spotitdeck() {
         let mut deck: SpotItDeck = SpotItDeck::new();
-        let card = SpotItCard(HashSet::from([SpotItSuit::Potato]));
+        let card = SpotItCard(HashSet::from([SpotItSymbol::Potato]));
         deck.cards.push(card.clone());
         assert_eq!(deck.pop_card(), Some(card));
         assert_eq!(deck.pop_card(), None);
@@ -317,5 +200,20 @@ mod tests {
         for card in deck.cards {
             assert_eq!(card.0.len(), 1);
         }
+    }
+    #[test]
+    fn can_generate_by_prime() {
+        let deck = SpotItDeck::generate_by_prime(1).unwrap();
+        assert_eq!(deck.cards.len(), 1 * 1 + 1);
+        let deck = SpotItDeck::generate_by_prime(2).unwrap();
+        assert_eq!(deck.cards.len(), 2 * 2 + 1);
+        let deck = SpotItDeck::generate_by_prime(3).unwrap();
+        assert_eq!(deck.cards.len(), 3 * 3 + 1);
+        let deck = SpotItDeck::generate_by_prime(4).unwrap();
+        assert_eq!(deck.cards.len(), 4 * 4 + 1);
+        let deck = SpotItDeck::generate_by_prime(5).unwrap();
+        assert_eq!(deck.cards.len(), 5 * 5 + 1);
+        // return error if n is too large
+        assert!(SpotItDeck::generate_by_prime(6).is_err());
     }
 }
